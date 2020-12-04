c91439550a5fe976bd9a7e228c636899beab73f4
Mayank <90mayanktyagi@gmail.com>
2020-12-04 10:47:50 +0530

    User data addition facility created

--- Code.cpp
+++ Code.cpp
@@ -21,6 +21,7 @@ public:
 	string getFirstName(){return firstName;}
 	string getLastName(){return lastName;}
 	float getBalance(){return balance;}
+	void InsufficientFunds(){cout<<"Low Balance\n";}
 	void Deposit(float amount);
 	void Withdraw(float amount);
 	static void setLastAccountNumber(long accountNumber);
@@ -124,7 +125,62 @@ int main()
 	}while(choice!=7);
 	return 0;
 }
-//Account::Account(string fna)
+Account::Account(string fname, string lname, float balance)
+{
+	NextAccountNumber++;
+	accountNumber=NextAccountNumber;
+	firstName=fname;
+	lastName=lname;
+	this->balance=balance;
+}
+void Account::Deposit(float amount)
+{
+	balance+=amount;
+}
+void Account::Withdraw(float amount)
+{
+	if(balace-amount<MIN_BALANCE)
+	{
+		throw InsufficientFunds();
+	}
+	balance-=amount;
+}
+void Account::setLastAccountNumber(long accountNumber)
+{
+	NextAccountNumber=accountNumber;
+}
+long Account::getLastAccountNumber()
+{
+	return NextAccountNumber;
+}
+ofstream & operator<<(ofstream &ofs, Account &acc)
+{
+	ofs<<acc.accountNumber<<endl;
+	ofs<<acc.firstName<<endl;
+	ofs<<acc.lastName<<endl;
+	ofs<<acc.balance<<endl;
+	return ofs;
+}
+ifstream & operator>>(ifstream &ifs, Account &acc)
+{
+	ifs>>acc.accountNumber;
+	ifs>>acc.firstName;
+	ifs>>acc.lastName;
+	ifs>>acc.balance;
+	return ifs;
+}
+ostream & operator<<(ostream &os, Account &acc)
+{
+	os<<"First Name:"<<acc.getFirstName()<<endl;
+	os<<"Last Name:"<<acc.getLastName()<<endl;
+	os<<"Account Number:"<<acc.getAccNo()<<endl;
+	os<<"Balance:"<<acc.getBalance()<<endl;
+	return os;
+}
 
+/*Bank::Bank()
+{
+
+}
 
 
